7d21e07a9d776a203eea1fae41f5056e087d9147
krishna-varik <krishnavarik9@gmail.com>
2021-11-21 15:09:42 +0530

    added domfile

---
+++ Desktop/Dom manipulation/dom.js
@@ -0,0 +1,15 @@
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+
+// console.log(document.forms[0]);
+
+
+//get Element ById
+var headerTitle = document.getElementById('header-title')
+console.log(headerTitle)
